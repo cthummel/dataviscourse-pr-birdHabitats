7d7a7a2b6d10de438a81561d4fdc2383764647d4
Corin Thummel <thummelcorin@gmail.com>
2019-11-05 12:58:03 -0700

    Added the year slider using code from homework 4

--- js/map.js
+++ js/map.js
@@ -3,10 +3,16 @@ class Map {
     constructor(data) {
         this.data = data;
         this.projection = null;
+        this.activeYear = 2016;
+        this.activeSeason = d3.scaleTime().domain([new Date(this.activeYear, 1, 1), new Date(this.activeYear, 12, 31)]).range([0,100])
         this.width = 960;
         this.height = 500;
 
+        console.log(this.activeSeason(new Date(this.activeYear, 2, 15)))
+
         this.initMap();
+        this.initializeSliders()
+        this.drawYearBar()
     }
 
     initMap() {
@@ -25,7 +31,7 @@ class Map {
                                 .center([50, 40])
                                 .rotate([105, 0])
                                 .parallels([35, 55])
-                                .scale(250)
+                                .scale(370)
                                 .translate([self.width / 2, self.height / 2]);
 
 
@@ -81,4 +87,75 @@ class Map {
                 }
             });
     }
+
+    initializeSliders()
+    {
+        d3.select("body").append("svg")
+                         .attr("width", this.width)
+                         .attr("height", 300).attr("id", "sliderSVG")
+                         .attr("translate", "transform(0, " + this.height + ")");
+
+        d3.select('body')
+          .append('div').attr('id', 'activeYear-bar');
+    }
+
+    drawYearBar() {
+
+        // ******* TODO: PART 2 *******
+        //The drop-down boxes are set up for you, but you have to set the slider to updatePlot() on activeYear change
+
+        // Create the x scale for the activeYear;
+        // hint: the domain should be max and min of the years (1800 - 2020); it's OK to set it as numbers
+        // the plot needs to update on move of the slider
+
+        /* ******* TODO: PART 3 *******
+        You will need to call the updateYear() function passed from script.js in your activeYear slider
+        */
+        let that = this;
+
+        //Slider to change the activeYear of the data
+        let yearScale = d3.scaleLinear().domain([1800, 2020]).range([30, 730]);
+
+        let yearSlider = d3.select('#activeYear-bar')
+            .append('div').classed('slider-wrap', true)
+            .append('input').classed('slider', true)
+            .attr('type', 'range')
+            .attr('min', 1800)
+            .attr('max', 2020)
+            .attr('value', this.activeYear);
+
+        let sliderLabel = d3.select('.slider-wrap')
+            .append('div').classed('slider-label', true)
+            .append('svg');
+
+        let sliderText = sliderLabel.append('text').text(this.activeYear);
+
+        sliderText.attr('x', yearScale(this.activeYear));
+        sliderText.attr('y', 25);
+
+        yearSlider.on('input', function() {
+            //YOUR CODE HERE 
+            that.activeYear = this.value;
+            d3.select("#activeYear-bar").select(".slider-wrap").select(".slider-label").select("text").attr("x", yearScale(that.activeYear)).text(that.activeYear)
+            //that.updatePlot(that.activeYear, xdrop, ydrop, cdrop);
+        });
+    }
+
+    displayRaster(birdname, displayType)
+    {
+        if(displayType == "abundance")
+        {
+
+        }
+        else if(displayType == "range")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+
 }
